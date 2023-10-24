1e93f5ccc9c3b1114e826b852aa85df7aff773bd
Muniraj M <107682409+Muniraj132@users.noreply.github.com>
2023-10-24 11:35:50 +0530

    implement the api in events page
    implement the api in events page

--- src/Pages/home/views/Events.jsx
+++ src/Pages/home/views/Events.jsx
@@ -1,65 +1,83 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import ApiUrl from "../../../Components/API/Api";
+import axios from "axios";
+import { useEffect } from "react";
 function Events() {
-    return (
-        <>
-            <section className="events">
-                <div className="container">
-                    <div className="session-title row">
-                        <h2>OUR CAUSES</h2>
-                        <p>
-                            We are a non-profital &amp; Charity raising money for child education
-                        </p>
-                    </div>
-                    <div className="event-ro row">
-                        <div className="col-md-4 col-sm-6">
-                            <div className="event-box">
-                                <img src="assets/images/events/image_08.jpg" alt="" />
-                                <h4>Child Education in Africa</h4>
-                                <p className="raises">
-                                    <span>Raised : 34,425</span> / 500,000
-                                </p>
-                                <p className="desic">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                                    industry. Lorem Ipsum has been the industry's
-                                </p>
-                                <button className="btn btn-success btn-sm">Donate Now</button>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-6">
-                            <div className="event-box">
-                                <img src="assets/images/events/image_06.jpg" alt="" />
-                                <h4>Child Education in Africa</h4>
-                                <p className="raises">
-                                    <span>Raised : 34,425</span> / 500,000
-                                </p>
-                                <p className="desic">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                                    industry. Lorem Ipsum has been the industry's
-                                </p>
-                                <button className="btn btn-success btn-sm">Donate Now</button>
-                            </div>
-                        </div>
-                        <div className="col-md-4 col-sm-6">
-                            <div className="event-box">
-                                <img src="assets/images/events/image_04.jpg" alt="" />
-                                <h4>Child Education in Africa</h4>
-                                <p className="raises">
-                                    <span>Raised : 34,425</span> / 500,000
-                                </p>
-                                <p className="desic">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                                    industry. Lorem Ipsum has been the industry's
-                                </p>
-                                <button className="btn btn-success btn-sm">Donate Now</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
+  const [causes, setcauses] = useState([]);
+  const [title, settitle] = useState([]);
+  const [description, setdescription] = useState([]);
 
-        </>
-    )
+  useEffect(() => {
+    const apiUrl = `${ApiUrl}/get/post/4`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setcauses(response.data.data);
+        settitle(response.data.category_name);
+        setdescription(response.data.category_description);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+  return (
+    <>
+      <section className="events">
+        <div className="container">
+          <div className="session-title row">
+            <h2>{title}</h2>
+            <p
+              style={{ textAlign: "center", width: "40%" }}
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+          </div>
+          <div className="event-ro row">
+            {causes.map((item) => (
+              <div className="col-md-4 col-sm-6">
+                <div className="event-box">
+                  <img src={item.image} alt="" />
+                  <h4>{item.title}</h4>
+                  <p
+                    className="desic"
+                    dangerouslySetInnerHTML={{ __html: item.content }}
+                  />
+                  <button className="btn btn-success btn-sm">Donate Now</button>
+                </div>
+              </div>
+            ))}
+            {/* <div className="col-md-4 col-sm-6">
+                <div className="event-box">
+                  <img src="assets/images/events/image_06.jpg" alt="" />
+                  <h4>Child Education in Africa</h4>
+                  <p className="raises">
+                    <span>Raised : 34,425</span> / 500,000
+                  </p>
+                  <p className="desic">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                  </p>
+                  <button className="btn btn-success btn-sm">Donate Now</button>
+                </div>
+              </div> */}
+            {/* <div className="col-md-4 col-sm-6">
+                <div className="event-box">
+                  <img src="assets/images/events/image_04.jpg" alt="" />
+                  <h4>Child Education in Africa</h4>
+                  <p className="raises">
+                    <span>Raised : 34,425</span> / 500,000
+                  </p>
+                  <p className="desic">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                  </p>
+                  <button className="btn btn-success btn-sm">Donate Now</button>
+                </div>
+              </div> */}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Events
+export default Events;
--- src/Pages/home/views/Slider.jsx
+++ src/Pages/home/views/Slider.jsx
@@ -1,44 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
-import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function Slider() {
-    const [slider, setSlider] = useState([]);
-    useEffect(() => {
-        const apiUrl = 'http://127.0.0.1:8000/api/get/slider';
-        axios.get(apiUrl)
-            .then(response => {
-                setSlider(response?.data?.data);
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
+  const [slider, setSlider] = useState([]);
+  useEffect(() => {
+    const apiUrl = "http://127.0.0.1:8000/api/get/slider";
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setSlider(response?.data?.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-    return (
-        <>
-            <Swiper
-                modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
-                slidesPerView={1}
-                navigation
-                loop={true}
-                pagination={{ clickable: true }}
-                centeredSlides={true}
-                autoplay={{
-                    delay: 5000,
-                    disableOnInteraction: false,
-                }}
-            > {slider?.map(item => (
-                <SwiperSlide> <div className="item">
-                    <div className="slider-img">
-                        <img src={item?.image} className='slider-img-height' width="2000px" alt="" />
-                    </div>
-                    <div className="container">
+  return (
+    <>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        slidesPerView={1}
+        navigation
+        loop={true}
+        pagination={{ clickable: true }}
+        centeredSlides={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}>
+        {" "}
+        {slider?.map((item) => (
+          <SwiperSlide>
+            <div className="item">
+              <div className="slider-img">
+                <img
+                  src={item?.image}
+                  className="slider-img-height"
+                  style={{ width: "100%" }}
+                  alt=""
+                />
+              </div>
+              {/* <div className="container">
                         <div className="row">
                             <div className="col-lg-offset-2 col-lg-8 col-md-offset-2 col-md-8 col-sm-12 col-xs-12">
                                 <div className="slider-captions">
@@ -49,12 +63,13 @@ function Slider() {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div> </SwiperSlide>
-            ))}
-            </Swiper>
-        </>
-    )
+                    </div> */}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
 
-export default Slider
+export default Slider;
